b2439ebc5be346a40df712906b9dbc0b6c96c4ac
estherff <76997049+estherff@users.noreply.github.com>
2021-01-26 20:34:44 +0100

    Incio Total

--- src/main/java/gal/teis/pgr/constructor/Libro.java
+++ src/main/java/gal/teis/pgr/constructor/Libro.java
@@ -2,48 +2,52 @@ package gal.teis.pgr.constructor;
 
 public class Libro {
 
-        //Propiedades
-	private String titulo;
-        private int paginas;
-        private String autor;
-        
-	private int totalPaginas;
-	private int paginaActual;
-        
-	
-	//Setter y getter
-        
-	public String getTitulo() {
-		return titulo;
-	}
-	public int getTotalPaginas() {
-		return totalPaginas;
-	}
-        public int getPaginaActual() {
-		return paginaActual;
-	}
-        public String getAutor() {
-		return autor;
-	}
-        /*No hay get para páginaActual, esta propiedad se maneja mediante un 
+    //Propiedades
+    private String titulo;
+    private int paginas;
+    private String autor;
+
+    private int totalPaginas;
+    private int paginaActual;
+
+    //Setter y getter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getTotalPaginas() {
+        return totalPaginas;
+    }
+
+    public int getPaginaActual() {
+        return paginaActual;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    /*No hay get para páginaActual, esta propiedad se maneja mediante un 
         método específico: avanzarPagina() y avanzarCapitulo()*/
-        
-        public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public void setTotalPaginas(int totalPaginas) {
-		this.totalPaginas = totalPaginas;
-	}
-        public void setAutor (String autor){
-            this.autor=autor;
-        }
-	
-	
-	//Métodos
-        public void avanzarPagina() {
-		paginaActual++;
-	}
-	public void avanzarCapitulo() {		
-		paginaActual += 20;
-	}
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setTotalPaginas(int totalPaginas) {
+        this.totalPaginas = totalPaginas;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    //Métodos
+    public void avanzarPagina() {
+        paginaActual++;
+    }
+
+    public void avanzarCapitulo() {
+        paginaActual += 20;
+    }
 }
--- src/main/java/gal/teis/pgr/constructor/Libro1.java
+++ src/main/java/gal/teis/pgr/constructor/Libro1.java
@@ -2,36 +2,38 @@ package gal.teis.pgr.constructor;
 
 public class Libro1 {
 
-        //Propiedades
-	private String titulo;
-        private String categoría;
-        private int paginas;
-        private String autor;
-        
-	private int totalPaginas;
-	private int paginaActual;
-        
-	
-	//Setter y getter
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public int getTotalPaginas() {
-		return totalPaginas;
-	}
-	
-	public int getPaginaActual() {
-		return paginaActual;
-	}
-	
-	//Métodos
-        public void avanzarPagina() {
-		paginaActual++;
-	}
-	public void avanzarCapitulo() {		
-		paginaActual += 20;
-	}
+    //Propiedades
+    private String titulo;
+    private String categoría;
+    private int paginas;
+    private String autor;
+
+    private int totalPaginas;
+    private int paginaActual;
+
+    //Setter y getter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalPaginas() {
+        return totalPaginas;
+    }
+
+    public int getPaginaActual() {
+        return paginaActual;
+    }
+
+    //Métodos
+    public void avanzarPagina() {
+        paginaActual++;
+    }
+
+    public void avanzarCapitulo() {
+        paginaActual += 20;
+    }
 }
